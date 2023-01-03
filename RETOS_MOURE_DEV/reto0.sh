5fe8cd2868269ba07e23d0ee580b12fc7d24e1a5
abarsi17 <abarsi@alumni.uv.es>
2023-01-03 18:26:48 +0100

    Añadir enunciado del programa

--- RETOS_MOURE_DEV/reto0.sh
+++ RETOS_MOURE_DEV/reto0.sh
@@ -1,5 +1,15 @@
 #! /bin/bash
 
+### 
+# Escribe un programa que muestre por consola (con un print) los
+# números de 1 a 100 (ambos incluidos y con un salto de línea entre
+# cada impresión), sustiuyendo los siguientes:
+# - Múltiplos de 3 por la palabra "fizz"
+# - Múltiplos de 5 por la palabra "buzz"
+# - Múltiplos de 3 y de 5 por la palabra "fizzbuzz"
+###
+
+
 # PRIMERA FORMA: utilizando let 
 echo -e "\nPRIMERA FORMA\n"
 
